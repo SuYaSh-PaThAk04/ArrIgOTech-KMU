c2f27f4797f8d11d24407a498b3598e1ef60f9f0
Suyash Pathak <suyashp271@gmail.com>
2026-02-19 02:05:02 +0530

    Delete server directory

--- server/src/routes/soil.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { analyzeSoil } from "../controller/soil.controller.js";
-
-const router = express.Router();
-router.post("/analyze", analyzeSoil);
-
-export default router;
